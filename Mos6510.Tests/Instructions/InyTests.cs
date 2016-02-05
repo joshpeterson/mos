b71cea2722ae1f080f9ef72228cc48482a70c2f7
Josh Peterson <petersonjm1@gmail.com>
2016-02-05 06:08:57 -0500

    Add tests for the status flags set by iny.
    There is no need for production code changes, as the status flag logic
    is already present in the RegisterUtils class.

--- Mos6510.Tests/Instructions/InyTests.cs
+++ Mos6510.Tests/Instructions/InyTests.cs
@@ -18,5 +18,33 @@ namespace Mos6510.Tests.Instructions
       Assert.That(model.GetRegister(RegisterName.Y).GetValue(),
                   Is.EqualTo(initialValue + 1));
     }
+
+    [TestCase(0x7F, true)]
+    [TestCase(0x7E, false)]
+    public void VerifyValuesOfNegativeFlag(int initialValue, bool expectedResult)
+    {
+      var model = new ProgrammingModel();
+      model.GetRegister(RegisterName.Y).SetValue(initialValue);
+      model.NegativeFlag = !expectedResult;
+
+      var instruction = new Iny();
+      instruction.Execute(model);
+
+      Assert.That(model.NegativeFlag, Is.EqualTo(expectedResult));
+    }
+
+    [TestCase(0xFF, true)]
+    [TestCase(0x00, false)]
+    public void VerifyValuesOfZeroFlag(int initialValue, bool expectedResult)
+    {
+      var model = new ProgrammingModel();
+      model.GetRegister(RegisterName.Y).SetValue(initialValue);
+      model.ZeroFlag = !expectedResult;
+
+      var instruction = new Iny();
+      instruction.Execute(model);
+
+      Assert.That(model.ZeroFlag, Is.EqualTo(expectedResult));
+    }
   }
 }
