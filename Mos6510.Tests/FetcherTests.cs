3035b82c59a12fb5abaa3a66c353a1530883f04a
Josh Peterson <petersonjm1@gmail.com>
2016-02-26 06:34:12 -0500

    The fetcher can now get the operand.

--- Mos6510.Tests/FetcherTests.cs
+++ Mos6510.Tests/FetcherTests.cs
@@ -1,24 +1,33 @@
 using NUnit.Framework;
 using Mos6510;
+using Mos6510.Instructions;
 
 namespace Mos6510.Tests
 {
   [TestFixture]
   public class FetcherTests
   {
+    private ProgrammingModel model;
+    private Memory memory;
+    private Fetcher fetcher;
+
+    [SetUp]
+    public void SetUp()
+    {
+      model = new ProgrammingModel();
+      memory = new Memory();
+      fetcher = new Fetcher(model, memory);
+    }
+
     [Test]
     public void FetchesTheNextInstructionFromTheProgramCounter()
     {
       const ushort expectedAddress = 0xFF10;
       const byte expectedValue = 0x2A;
 
-      var model = new ProgrammingModel();
       model.GetRegister(RegisterName.PC).SetValue(expectedAddress);
-
-      var memory = new Memory();
       memory.SetValue(expectedAddress, expectedValue);
 
-      var fetcher = new Fetcher(model, memory);
       Assert.That(fetcher.Fetch(), Is.EqualTo(expectedValue));
     }
 
@@ -27,15 +36,80 @@ namespace Mos6510.Tests
     {
       const ushort initialAddress = 0xFF10;
 
-      var model = new ProgrammingModel();
       model.GetRegister(RegisterName.PC).SetValue(initialAddress);
 
-      var memory = new Memory();
-
-      var fetcher = new Fetcher(model, memory);
       fetcher.Fetch();
       Assert.That(model.GetRegister(RegisterName.PC).GetValue(),
           Is.EqualTo(initialAddress + 1));
+    }
+
+    [TestCase(AddressingMode.Immediate)]
+    [TestCase(AddressingMode.Zeropage)]
+    [TestCase(AddressingMode.ZeropageX)]
+    [TestCase(AddressingMode.ZeropageY)]
+    [TestCase(AddressingMode.IndexedIndirectX)]
+    [TestCase(AddressingMode.IndexedIndirectY)]
+    public void GetsOneByteTheOperandFor(AddressingMode mode)
+    {
+      const ushort address = 0xFF10;
+      const byte expectedOperand = 0xC2;
+
+      model.GetRegister(RegisterName.PC).SetValue(address);
+      memory.SetValue(address + 1, expectedOperand);
+      fetcher.Fetch();
+
+      Assert.That(fetcher.OperandFor(mode), Is.EqualTo(expectedOperand));
+    }
+
+    [TestCase(AddressingMode.Immediate)]
+    [TestCase(AddressingMode.Zeropage)]
+    [TestCase(AddressingMode.ZeropageX)]
+    [TestCase(AddressingMode.ZeropageY)]
+    [TestCase(AddressingMode.IndexedIndirectX)]
+    [TestCase(AddressingMode.IndexedIndirectY)]
+    public void IncrementsThePCAferReadingAOneByteOperand(AddressingMode mode)
+    {
+      const ushort initialAddress = 0xFF10;
+
+      model.GetRegister(RegisterName.PC).SetValue(initialAddress);
+      fetcher.Fetch();
+
+      fetcher.OperandFor(mode);
+      Assert.That(model.GetRegister(RegisterName.PC).GetValue(),
+          Is.EqualTo(initialAddress + 2));
+    }
+
+    [TestCase(AddressingMode.Absolute)]
+    [TestCase(AddressingMode.AbsoluteX)]
+    [TestCase(AddressingMode.AbsoluteY)]
+    public void GetsTwoByteTheOperandFor(AddressingMode mode)
+    {
+      const ushort address = 0xFF10;
+      const byte operandLow = 0xC2;
+      const byte operandHigh = 0x59;
+      ushort expectedOperand = (operandHigh << 8) | operandLow;
+
+      model.GetRegister(RegisterName.PC).SetValue(address);
+      memory.SetValue(address + 1, operandLow);
+      memory.SetValue(address + 2, operandHigh);
+      fetcher.Fetch();
+
+      Assert.That(fetcher.OperandFor(mode), Is.EqualTo(expectedOperand));
+    }
+
+    [TestCase(AddressingMode.Absolute)]
+    [TestCase(AddressingMode.AbsoluteX)]
+    [TestCase(AddressingMode.AbsoluteY)]
+    public void IncrementsThePCAferReadingATwoByteOperand(AddressingMode mode)
+    {
+      const ushort initialAddress = 0xFF10;
+
+      model.GetRegister(RegisterName.PC).SetValue(initialAddress);
+      fetcher.Fetch();
+
+      fetcher.OperandFor(mode);
+      Assert.That(model.GetRegister(RegisterName.PC).GetValue(),
+          Is.EqualTo(initialAddress + 3));
     }
   }
 }
--- Mos6510/Fetcher.cs
+++ Mos6510/Fetcher.cs
@@ -1,3 +1,5 @@
+using Mos6510.Instructions;
+
 namespace Mos6510
 {
   public class Fetcher
@@ -12,6 +14,25 @@ namespace Mos6510
     }
 
     public byte Fetch()
+    {
+      return ReadPCAndIncrement();
+    }
+
+    public ushort OperandFor(AddressingMode mode)
+    {
+      ushort operand = ReadPCAndIncrement();
+      if (mode == AddressingMode.Absolute ||
+          mode == AddressingMode.AbsoluteX ||
+          mode == AddressingMode.AbsoluteY)
+      {
+        byte operandHigh = ReadPCAndIncrement();
+        operand = (ushort)((operandHigh << 8) | operand);
+      }
+
+      return operand;
+    }
+
+    private byte ReadPCAndIncrement()
     {
       var pc = model.GetRegister(RegisterName.PC);
       var address = pc.GetValue();
