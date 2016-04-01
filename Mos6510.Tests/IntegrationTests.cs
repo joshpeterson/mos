283110796f83b3227ad7953f98b70f78b909ea66
Josh Peterson <petersonjm1@gmail.com>
2016-04-01 06:19:36 -0400

    Add the And commands to the registry

--- Mos6510.Tests/IntegrationTests.cs
+++ Mos6510.Tests/IntegrationTests.cs
@@ -1,19 +1,56 @@
 using NUnit.Framework;
+using Mos6510;
+using System.Collections;
 
 namespace Mos6510.Tests
 {
   [TestFixture]
   public class IntegrationTests
   {
-    [TestCase("Inx", "X:  0x01")]
-    [TestCase("Iny", "Y:  0x01")]
-    public void InstructionWorksEndToEnd(string input, string expectedOutput)
-    {
-      var repl = new Repl(new ProgrammingModel(), new Memory());
+    public delegate void SetUp(ProgrammingModel model, Memory memory);
 
-      repl.TryRead(input);
-      repl.Execute();
+    public static object[] TestCases =
+    {
+        new TestCaseData(new SetUp(Empty), "Inx", "X:  0x01"),
+        new TestCaseData(new SetUp(Empty), "Iny", "Y:  0x01"),
+        new TestCaseData(new SetUp(InitializeForAnd), "And #$80", "A:  0x80"),
+        new TestCaseData(new SetUp(InitializeForAnd), "And $1000", "A:  0x80"),
+        new TestCaseData(new SetUp(InitializeForAnd), "And $FF0,X", "A:  0x80"),
+        new TestCaseData(new SetUp(InitializeForAnd), "And $FF0,Y", "A:  0x80"),
+        new TestCaseData(new SetUp(InitializeForAnd), "And $10", "A:  0x80"),
+        new TestCaseData(new SetUp(InitializeForAnd), "And $00,X", "A:  0x80"),
+        new TestCaseData(new SetUp(InitializeForAnd), "And ($10,X)", "A:  0x80"),
+        new TestCaseData(new SetUp(InitializeForAnd), "And ($10),Y", "A:  0x80"),
+    };
+
+    [TestCaseSource("TestCases")]
+    public void InstructionWorksEndToEnd(SetUp setup, string input,
+                                         string expectedOutput)
+    {
+      var model = new ProgrammingModel();
+      var memory = new Memory();
+      setup(model, memory);
+      var repl = new Repl(model, memory);
+
+      if (!repl.TryRead(input))
+        Assert.Fail(string.Format("Unable to read assembly input: '{0}'", input));
+      if (!repl.Execute())
+        Assert.Fail(string.Format("Unable to execute input: '{0}'", input));
       Assert.That(repl.PrintRegisters(), Is.StringContaining(expectedOutput));
+    }
+
+    private static void Empty(ProgrammingModel model, Memory memory)
+    {
+    }
+
+    private static void InitializeForAnd(ProgrammingModel model, Memory memory)
+    {
+      model.GetRegister(RegisterName.A).SetValue(0xFF);
+      model.GetRegister(RegisterName.X).SetValue(0x10);
+      model.GetRegister(RegisterName.Y).SetValue(0x10);
+      memory.SetValue(0x1000, 0x80);
+      memory.SetValue(0x10, 0x80);
+      memory.SetValue(0x21, 0x10);
     }
   }
 }
--- Mos6510/InstructionRegistry.cs
+++ Mos6510/InstructionRegistry.cs
@@ -8,6 +8,14 @@ namespace Mos6510
     {
       { 0xE0, Opcode.Inx, new Inx(), AddressingMode.Implied },
       { 0xC0, Opcode.Iny, new Iny(), AddressingMode.Implied },
+      { 0x29, Opcode.And, new And(), AddressingMode.Immediate },
+      { 0x2D, Opcode.And, new And(), AddressingMode.Absolute },
+      { 0x3D, Opcode.And, new And(), AddressingMode.AbsoluteX },
+      { 0x39, Opcode.And, new And(), AddressingMode.AbsoluteY },
+      { 0x25, Opcode.And, new And(), AddressingMode.Zeropage },
+      { 0x35, Opcode.And, new And(), AddressingMode.ZeropageX },
+      { 0x21, Opcode.And, new And(), AddressingMode.IndexedIndirectX },
+      { 0x31, Opcode.And, new And(), AddressingMode.IndexedIndirectY },
     };
   }
 }
