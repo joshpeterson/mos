2424ae7e8011fda65429cd01b0030b4ede8774ad
Josh Peterson <petersonjm1@gmail.com>
2017-07-06 06:48:37 -0400

    Implement the Bne instruction

---
+++ Mos6510.Tests/Instructions/BneTests.cs
@@ -0,0 +1,113 @@
+using NUnit.Framework;
+using Mos6510.Instructions;
+
+namespace Mos6510.Tests.Instructions
+{
+  [TestFixture]
+  public class BneTests
+  {
+    [Test]
+    public void SetsPCWhenZeroFlagIsClear()
+    {
+      var model = new ProgrammingModel();
+      var programCounter = model.GetRegister(RegisterName.PC);
+      programCounter.SetValue(0xC000);
+      model.ZeroFlag = false;
+
+      const ushort branchTarget = 0xD000;
+      new Bne().Execute(model, null, new Argument(0, branchTarget));
+      Assert.That(programCounter.GetValue(), Is.EqualTo(branchTarget));
+    }
+
+    [Test]
+    public void DoesNotSetPCWhenZeroFlagIsSet()
+    {
+      var model = new ProgrammingModel();
+      var programCounter = model.GetRegister(RegisterName.PC);
+      const ushort previousPCValue = 0xC000;
+      programCounter.SetValue(previousPCValue);
+      model.ZeroFlag = true;
+
+      new Bne().Execute(model, null, new Argument(0, 0xD000));
+      Assert.That(programCounter.GetValue(), Is.EqualTo(previousPCValue));
+    }
+
+    [Test]
+    public void ReturnsSuccessWhenTheZeroFlagIsClear()
+    {
+      var model = new ProgrammingModel();
+      var programCounter = model.GetRegister(RegisterName.PC);
+      programCounter.SetValue(0xC000);
+      model.ZeroFlag = false;
+
+      const ushort branchTarget = 0xD000;
+      var result = new Bne().Execute(model, null, new Argument(0, branchTarget));
+      Assert.That(result.HasFlag(Result.Success));
+    }
+
+    [Test]
+    public void ReturnsSuccessWhenTheZeroFlagIsSet()
+    {
+      var model = new ProgrammingModel();
+      var programCounter = model.GetRegister(RegisterName.PC);
+      programCounter.SetValue(0xC000);
+      model.ZeroFlag = true;
+
+      const ushort branchTarget = 0xD000;
+      var result = new Bne().Execute(model, null, new Argument(0, branchTarget));
+      Assert.That(result.HasFlag(Result.Success));
+    }
+
+    [Test]
+    public void ReturnsBranchTakenWhenTheZeroFlagIsClear()
+    {
+      var model = new ProgrammingModel();
+      var programCounter = model.GetRegister(RegisterName.PC);
+      programCounter.SetValue(0xC000);
+      model.ZeroFlag = false;
+
+      const ushort branchTarget = 0xD000;
+      var result = new Bne().Execute(model, null, new Argument(0, branchTarget));
+      Assert.That(result.HasFlag(Result.BranchTaken));
+    }
+
+    [Test]
+    public void DoesNotReturnsBranchTakenWhenTheZeroFlagIsSet()
+    {
+      var model = new ProgrammingModel();
+      var programCounter = model.GetRegister(RegisterName.PC);
+      programCounter.SetValue(0xC000);
+      model.ZeroFlag = true;
+
+      const ushort branchTarget = 0xD000;
+      var result = new Bne().Execute(model, null, new Argument(0, branchTarget));
+      Assert.That(!result.HasFlag(Result.BranchTaken));
+    }
+
+    [Test]
+    public void ReturnsBranchAcrossPageBoundaryWhenBranchIsFarEnough()
+    {
+      var model = new ProgrammingModel();
+      var programCounter = model.GetRegister(RegisterName.PC);
+      programCounter.SetValue(0xC000);
+      model.ZeroFlag = false;
+
+      const ushort branchTarget = 0xD000;
+      var result = new Bne().Execute(model, null, new Argument(0, branchTarget));
+      Assert.That(result.HasFlag(Result.BranchAcrossPageBoundary));
+    }
+
+    [Test]
+    public void DoesNotReturnBranchAcrossPageBoundaryWhenBranchIsCloseEnough()
+    {
+      var model = new ProgrammingModel();
+      var programCounter = model.GetRegister(RegisterName.PC);
+      programCounter.SetValue(0xC000);
+      model.ZeroFlag = false;
+
+      const ushort branchTarget = 0xC100;
+      var result = new Bne().Execute(model, null, new Argument(0, branchTarget));
+      Assert.That(!result.HasFlag(Result.BranchAcrossPageBoundary));
+    }
+  }
+}
